7e8ab38df0815032608f618227ab4e1d7a678350
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-10-18 22:52:51 +0530

    Update 1.py

--- codesprint/1.py
+++ codesprint/1.py
@@ -6,7 +6,6 @@ def minimumTime(x):
     #  Return the minimum time needed to visit all the houses.
     l = x.sort()
     s = 0
-   
     for i in range (len(x)-1) :
 	s += abs(x[i]-x[i+1])
         
